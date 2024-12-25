5ef7d030b8114535e741997fc7fbb27c659b0e5c
Dmytro Varchuk <loltop792@gmail.com>
2024-12-25 19:13:39 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	const int SIZE = 6;
+	int income[SIZE];
+	int result = 0;
+	for (int i = 0; i <= SIZE - 1; i++) {
+		cout << "Enter income for month " << i + 1 << ": ";
+		cin >> income[i];
+		result += income[i];
+	}
+	cout << "Total income for the half of year is: " << result << endl;
+	return 0;
+}
